edce6fa7b0ff1791bf05857b6aca34fd0825e4ad
Adam Christian <adam.christian@gmail.com>
2010-05-07 16:58:54 -0700

    Enabling flex actions ondemand

--- extension/chrome/content/extensions/extension-flexpilot.js
+++ extension/chrome/content/extensions/extension-flexpilot.js
@@ -19,45 +19,54 @@ Copyright 2009-2010, Sauce Labs
 
 //x.doFlashType("id=devMovie","id:howdyButton")
 Selenium.prototype.doFlexClick = function(locator, flashLoc) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_click'](strToObj(flashLoc));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+//Enable this command in the SauceRC
+RemoteSelenium.prototype.doFlexClick = function(locator, flashLoc) {
+   return this.doCommand("flexClick", [locator, flashLoc], this.handleResults);
 };
 
 Selenium.prototype.doFlexDoubleClick = function(locator, flashLoc) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_doubleClick'](strToObj(flashLoc));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexDoubleClick = function(locator, flashLoc) {
+  return this.doCommand("flexDoubleClick", [locator, flashLoc], this.handleResults);
 };
 
 //x.doFlashType("id=devMovie","chain=name:testTextArea/name:UITextField18", "SUP MANGO")
@@ -65,176 +74,207 @@ Selenium.prototype.doFlexDoubleClick = function(locator, flashLoc) {
 // target = id=devMovie
 // value = {flash:"chain=name:testTextArea/name:UITextField18", text:"ROCK"}
 Selenium.prototype.doFlexType = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_type'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexType = function(locator, options) {
+  return this.doCommand("flexType", [locator, options], this.handleResults);
 };
 
 //x.doFlashSelect("id=devMovie", "chain=id:subPanel/name:comboTest", "label=Alex")
 //{option:"label=Alex","flash":"chain=id:subPanel/name:comboTest"}
 Selenium.prototype.doFlexSelect = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   //Lookup the flash movie
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_select'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexSelect = function(locator, options) {
+  return this.doCommand("flexSelect", [locator, options], this.handleResults);
 };
 
 //x.doFlashDragDropElemToElem("id=devMovie", "chain=name:dragSprite", "optchain=id:subPanel")
 //{chain:"name:dragSprite", optchain:"name:testTextArea"}
 Selenium.prototype.doFlexDragDropElemToElem = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_dragDropElemToElem'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexDragDropElemToElem = function(locator, options) {
+  return this.doCommand("flexDragDropElemToElem", [locator, options], this.handleResults);
 };
 
 //x.doFlashDragDropElemToElem("id=devMovie", "chain=name:dragSprite", "optchain=id:subPanel")
 Selenium.prototype.doFlexDragDropToCoords = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_dragDropToCoords'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexDragDropToCoords = function(locator, options) {
+  return this.doCommand("flexDragDropToCoords", [locator, options], this.handleResults);
 };
 
 Selenium.prototype.doFlexAssertDisplayObject = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_assertDisplayObject'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
 };
 
+RemoteSelenium.prototype.doFlexAssertDisplayObject = function(locator, options) {
+  return this.doCommand("flexAssertDisplayObject", [locator, options], this.handleResults);
+};
 
 Selenium.prototype.doFlexAssertTextIn = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_assertTextIn'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexAssertTextIn = function(locator, options) {
+  return this.doCommand("flexAssertTextIn", [locator, options], this.handleResults);
 };
 
 Selenium.prototype.doFlexAssertText = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_assertText'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexAssertText = function(locator, options) {
+  return this.doCommand("flexAssertText", [locator, options], this.handleResults);
 };
 
 Selenium.prototype.doFlexAssertProperty = function(locator, options) {
-	var strToObj = function(str){
-		var obj = {};
-		try { obj = eval("(" + str + ")") }
-		catch(err) {
-			var regExp = /\s+/g;
-			str = str.replace(regExp,'')
-			var optArr = str.split(",")
-			for (var i=0;i<optArr.length;i++){
-				var entryArr = optArr[i].split("=");
-				obj[entryArr[0]] = entryArr[1];
-			}
-		}
-		return obj; 
-	}
+  var strToObj = function(str){
+    var obj = {};
+    try { obj = eval("(" + str + ")") }
+    catch(err) {
+      var regExp = /\s+/g;
+      str = str.replace(regExp,'')
+      var optArr = str.split(",")
+      for (var i=0;i<optArr.length;i++){
+        var entryArr = optArr[i].split("=");
+        obj[entryArr[0]] = entryArr[1];
+      }
+    }
+    return obj; 
+  }
   
   var movie = this.browserbot.findElement(locator);
   var res = movie.wrappedJSObject['fp_assertProperty'](strToObj(options));
-	if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+  if (typeof(res) == "object"){ throw new SeleniumError(res.message); }
+};
+
+RemoteSelenium.prototype.doFlexAssertProperty = function(locator, options) {
+  return this.doCommand("flexAssertProperty", [locator, options], this.handleResults);
 };
